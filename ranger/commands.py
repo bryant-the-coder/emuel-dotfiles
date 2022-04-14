09df77c957319afc3e4ac2cea75789f3bf1bf1d0
emuel-vassallo <emuelvassallo15@gmail.com>
2022-04-14 17:13:56 +0200

    Generated default config files

--- ranger/commands.py
+++ ranger/commands.py
@@ -1,156 +1,62 @@
-from ranger.gui.colorscheme import ColorScheme
-from ranger.gui.color import *
+# This is a sample commands.py.  You can add your own commands here.
+#
+# Please refer to commands_full.py for all the default commands and a complete
+# documentation.  Do NOT add them all here, or you may end up with defunct
+# commands when upgrading ranger.
 
-class GoodNightAmerica(ColorScheme):
-    progress_bar_color = white
+# A simple command for demonstration purposes follows.
+# -----------------------------------------------------------------------------
 
-    def use(self, context):
-        fg, bg, attr = default_colors
+from __future__ import (absolute_import, division, print_function)
 
-        if context.reset:
-            return default_colors
+# You can import any python module as needed.
+import os
 
-        elif context.in_browser:
-            if context.selected:
-                attr = reverse
-            else:
-                attr = normal
-            if context.empty or context.error:
-                fg = black
-                bg = white
-            if context.border:
-                fg = white
-            if context.image:
-                fg = cyan
-            if context.video:
-                fg = cyan
-            if context.audio:
-                fg = cyan
-            if context.document:
-                fg = black
-            if context.container:
-                attr |= bold
-                fg = red
-            if context.directory:
-                attr |= normal
-                fg = white
-            elif context.executable and not \
-                    any((context.media, context.container,
-                       context.fifo, context.socket)):
-                attr |= bold
-                fg = red
-            if context.socket:
-                fg = cyan
-                attr |= bold
-            if context.fifo or context.device:
-                fg = cyan
-                if context.device:
-                    attr |= bold
-            if context.link:
-                fg = context.good and red or blue
-            if context.bad:
-                fg = red
-            if context.tag_marker and not context.selected:
-                attr |= bold
-                if fg in (red, white):
-                    fg = blue
-                else:
-                    fg = cyan
-            if not context.selected and (context.cut or context.copied):
-                fg = cyan
-                bg = black
-            if context.main_column:
-                if context.selected:
-                    attr |= bold
-                if context.marked:
-                    attr |= bold
-                    fg = black
-            if context.badinfo:
-                if attr & reverse:
-                    bg = red
-                else:
-                    fg = white
-
-        elif context.in_titlebar:
-            attr |= bold
-            if context.hostname:
-                fg = context.bad and red or cyan
-            elif context.directory:
-                fg = red
-            elif context.tab:
-                if context.good:
-                    bg = blue
-            elif context.link:
-                fg = white
-
-        elif context.in_statusbar:
-            if context.permissions:
-                if context.good:
-                    fg = cyan
-                    bg = black
-                elif context.bad:
-                    fg = red
-            if context.marked:
-                attr |= bold | reverse
-                fg = black
-            if context.message:
-                if context.bad:
-                    attr |= bold
-                    fg = cyan
-            if context.loaded:
-                bg = self.progress_bar_color
-            if context.vcsinfo:
-                fg = blue
-                attr &= ~bold
-            if context.vcscommit:
-                fg = blue
-                attr &= ~bold
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import Command
 
 
-        if context.text:
-            if context.highlight:
-                attr |= reverse
+# Any class that is a subclass of "Command" will be integrated into ranger as a
+# command.  Try typing ":my_edit<ENTER>" in ranger!
+class my_edit(Command):
+    # The so-called doc-string of the class will be visible in the built-in
+    # help that is accessible by typing "?c" inside ranger.
+    """:my_edit <filename>
 
-        if context.in_taskview:
-            if context.title:
-                fg = blue
+    A sample command for demonstration purposes that opens a file in an editor.
+    """
 
-            if context.selected:
-                attr |= reverse
+    # The execute method is called when you run this command in ranger.
+    def execute(self):
+        # self.arg(1) is the first (space-separated) argument to the function.
+        # This way you can write ":my_edit somefilename<ENTER>".
+        if self.arg(1):
+            # self.rest(1) contains self.arg(1) and everything that follows
+            target_filename = self.rest(1)
+        else:
+            # self.fm is a ranger.core.filemanager.FileManager object and gives
+            # you access to internals of ranger.
+            # self.fm.thisfile is a ranger.container.file.File object and is a
+            # reference to the currently selected file.
+            target_filename = self.fm.thisfile.path
 
-            if context.loaded:
-                if context.selected:
-                    fg = self.progress_bar_color
-                else:
-                    bg = self.progress_bar_color
+        # This is a generic function to print text in ranger.
+        self.fm.notify("Let's edit the file " + target_filename + "!")
 
+        # Using bad=True in fm.notify allows you to print error messages:
+        if not os.path.exists(target_filename):
+            self.fm.notify("The given file does not exist!", bad=True)
+            return
 
-        if context.vcsfile and not context.selected:
-            attr &= ~bold
-            if context.vcsconflict:
-                fg = cyan
-            elif context.vcschanged:
-                fg = blue
-            elif context.vcsunknown:
-                fg = red
-            elif context.vcsstaged:
-                fg = white
-            elif context.vcssync:
-                fg = black
-            elif context.vcsignored:
-                fg = default
+        # This executes a function from ranger.core.acitons, a module with a
+        # variety of subroutines that can help you construct commands.
+        # Check out the source, or run "pydoc ranger.core.actions" for a list.
+        self.fm.edit_file(target_filename)
 
-        elif context.vcsremote and not context.selected:
-            attr &= ~bold
-            if context.vcssync:
-                fg = cyan
-            elif context.vcsbehind:
-                fg = blue
-            elif context.vcsahead:
-                fg = red
-            elif context.vcsdiverged:
-                fg = white
-            elif context.vcsunknown:
-                fg = black
-
-        return fg, bg, attr
+    # The tab method is called when you press tab, and should return a list of
+    # suggestions that the user will tab through.
+    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
+    def tab(self, tabnum):
+        # This is a generic tab-completion function that iterates through the
+        # content of the current directory.
+        return self._tab_directory_content()
