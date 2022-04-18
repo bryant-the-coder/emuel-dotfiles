c545e9d37691bea04ae1a9e729ff0477add7bae5
emuel-vassallo <emuelvassallo15@gmail.com>
2022-04-18 09:41:53 +0200

    Moved places of elements and changed section dividers

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -11,10 +11,10 @@ local diagnostics = {
 	"diagnostics",
 	sources = { "nvim_diagnostic" },
 	sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
+	symbols = { error = " ", warn = " " },
 	colored = false,
 	update_in_insert = false,
-	always_visible = true,
+	always_visible = false,
 }
 
 local diff = {
@@ -27,14 +27,17 @@ local diff = {
 local mode = {
 	"mode",
 	fmt = function(str)
-		return "-- " .. str .. " --"
+		return str
 	end,
+  padding = 1.5
 }
-
+    
 local filetype = {
 	"filetype",
-	icons_enabled = false,
-	icon = nil,
+	icons_enabled = true,
+  colored = false,
+  icon_only = true,
+  padding = 1,
 }
 
 local branch = {
@@ -43,19 +46,26 @@ local branch = {
 	icon = "",
 }
 
-local location = {
-	"location",
-	padding = 0,
+local filename = {
+  'filename',
+    file_status = true,      -- Displays file status (readonly status, modified status)
+    path = 0,                -- 0: Just the filename
+                             -- 1: Relative path
+                             -- 2: Absolute path
+
+    shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                             -- for other components. (terrible name, any suggestions?)
+  padding = 1,
 }
 
 -- cool function for progress
 local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+local current_line = vim.fn.line(".")
+local total_lines = vim.fn.line("$")
+local line_ratio = current_line / total_lines
+local percentage = math.floor(line_ratio * 100)
+local symbol = "٪"
+return percentage .. symbol
 end
 
 local spaces = function()
@@ -67,27 +77,27 @@ lualine.setup({
 		icons_enabled = true,
 		theme = "auto",
 		component_separators = { left = "", right = "" },
-		section_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
 	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
+		lualine_a = { mode },
+		lualine_b = { filetype, filename },
+    lualine_c = { diff },
 		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
-		lualine_y = { location },
+		lualine_x = { branch },
+		lualine_y = { diagnostics },
 		lualine_z = { progress },
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
+		lualine_c = {},
 		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {},
 	},
 	tabline = {},
-	extensions = {},
+	extensions = {"toggleterm", "nvim-tree"},
 })
